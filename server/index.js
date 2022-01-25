9a98342a6e6a9a1127bf136e252a52f90b83d07e
Ketan Jingar <keith212005@gmail.com>
2022-01-25 23:35:35 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const db = require('./queries');
+
+const port = process.env.PORT || 5000;
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' });
+});
+
+app.get('/users', db.getUsers);
+app.get('/users/:id', db.getUserById);
+app.post('/users', db.createUser);
+app.put('/users/:id', db.updateUser);
+app.put('/users/:id', db.deleteUser);
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
---
+++ server/utils/constants.js
@@ -0,0 +1,10 @@
+const routes = {
+  home: '/',
+  users: '/users',
+  singleUser: '/users/:id',
+  createUser: 'users',
+  updateUser: '/users/:id',
+  deleteUser: '/users/:id',
+};
+
+module.exports = { routes };
